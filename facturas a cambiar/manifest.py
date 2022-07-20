633250a0a7445287b8dbcc4037716490f1b5094d
mvnsmartify <98100153+mvnsmartify@users.noreply.github.com>
2022-07-20 15:19:12 -0400

    carga de objeto de facturas a cambiar

---
+++ facturas a cambiar/manifest.py
@@ -0,0 +1,14 @@
+{
+    'name': "Facturas a cambiar(Rep. Dominicana)",
+    'summary': """
+        Este módulo implementa la administración y gestión de los números de
+         comprobantes fiscales para el cumplimento de la norma 06-18 de la
+         Dirección de Impuestos Internos en la República Dominicana.""",
+    'author': "Malvin, " "Smartify",
+    'category': 'Localization',
+    'license': 'LGPL-3',
+    'version': "13.0.1.0.0",
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
